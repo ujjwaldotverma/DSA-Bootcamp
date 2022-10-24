6609a3da6b4f43444722e83d6877ba0e94036f14
ujjwaldotverma <72538299+ujjwaldotverma@users.noreply.github.com>
2022-10-24 13:03:40 +0530

    Update reverse.py

--- reverse.py
+++ reverse.py
@@ -2,7 +2,7 @@ arr=[1,2,3,4,5]
 left = 0
 right = len(arr)-1
 while left<right:
-    arr[left],arr[right]=arr[right],arr[left]
+    arr[left],arr[right]=arr[right],arr[left]                 #swapping
     left+=1 
     right-=1 
 print(arr)
