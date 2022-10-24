74f6018ded0f4cdc8b246d19f5985d3f9fc29e50
ujjwaldotverma <72538299+ujjwaldotverma@users.noreply.github.com>
2022-10-24 13:35:36 +0530

    Update KthSmallestUsingMaxHeap.py

--- Array/KthSmallestUsingMaxHeap.py
+++ Array/KthSmallestUsingMaxHeap.py
@@ -1,1 +1,34 @@
+from heapq import heapify,heappush,heappop
+class Solution:
+    def kthSmallest(self,arr, l, r, k):
+        '''
+        arr : given array
+        l : starting index of the array i.e 0
+        r : ending index of the array i.e size-1
+        k : find kth smallest element and return using this function
+        '''
+        q=[]
+        heapify(q)
+        for i in range(k):
+            heappush(q,-1*arr[i])
+        for i in range(k,len(arr)):
+            x=-1*heappop(q)
+            if arr[i]<x:
+                heappush(q,arr[i]*-1)
+            else:
+                heappush(q,-1*x)
+        
+        return -1*heappop(q) 
 
+#main fn
+
+if __name__ == '__main__': 
+    import random 
+    t=int(input())
+    for tcs in range(t):
+        n=int(input())
+        arr=list(map(int,input().strip().split()))
+        k=int(input())
+        ob=Solution()
+        print(ob.kthSmallest(arr, 0, n-1, k))
+        
