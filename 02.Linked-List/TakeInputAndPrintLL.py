6827eaf592c98d2a6e48ae6253f8eb33d35084e8
ujjwaldotverma <72538299+ujjwaldotverma@users.noreply.github.com>
2022-10-25 12:07:17 +0530

    Create TakeInputAndPrintLL.py

---
+++ 02.Linked-List/TakeInputAndPrintLL.py
@@ -0,0 +1,46 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+        
+def printLL(head):
+    while head is not None:
+        print(str(head.data)+"->",end="")
+        head = head.next
+    print("None")
+    return
+  
+def takeInput():
+    inputList = [int(ele) for ele in input().split()]
+    head = None
+    for currData in inputList:
+        if currData == -1:
+            break
+        newNode = Node(currData)
+        if head is None:
+            head = newNode
+        else:
+            curr = head
+            while curr.next is not None:              #TC = O(n^2)
+                curr = curr.next
+            curr.next = newNode
+    return head
+#OR
+def takeInput():
+    inputList = [int(ele) for ele in input().split()]
+    head = None
+    tail = None
+    for currData in inputList:
+        if currData == -1:
+            break
+        newNode = Node(currData)
+        if head is None:
+            head = newNode
+            tail = newNode
+        else:
+            tail.next = newNode                       #TC = O(n)
+            tail = newNode
+    return head    
+
+head = takeInput()
+printLL(head)
