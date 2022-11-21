936ddd13b6c5e50c7423bd0f2739a68d72080ddd
ujjwaldotverma <72538299+ujjwaldotverma@users.noreply.github.com>
2022-11-21 13:38:34 +0530

    added 2nd approach

--- 01.Array/11.ContainsDuplicate.py
+++ 01.Array/11.ContainsDuplicate.py
@@ -9,3 +9,12 @@ class Solution:
         return False
 #TC=O(n)
 #SC=O(n)
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        nums.sort()
+        for i in range(1,len(nums)):
+            if nums[i] == nums[i-1]:
+                return True
+        return False
+#TC=O(nlogn)
+#SC=O(1)
