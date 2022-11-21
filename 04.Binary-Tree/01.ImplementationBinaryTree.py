36b77509cb2ad8a1ac4c52c5f28be71bc8b96e87
ujjwaldotverma <72538299+ujjwaldotverma@users.noreply.github.com>
2022-11-21 23:51:37 +0530

    added more functions

--- 04.Binary-Tree/01.ImplementationBinaryTree.py
+++ 04.Binary-Tree/01.ImplementationBinaryTree.py
@@ -33,6 +33,49 @@ def treeInput():                            #ask user for input
     root.left = leftTree
     root.right = rightTree
     return root
+ 
+def numLeafNodes(root):                             #fn to find no. of leaf nodes
+    if root == None:
+        return 0
+    if root.left == None and root.right == None:
+        return 1
+    numLeftNodes = numLeftNodes(root.left)
+    numRightNodes = numRightNodes(root.right)
+    return numLeftNodes + numRightNodes
+
+def printDepthK(root,k):                            #fn to print nodes at depth k
+    if root == None:
+        return
+    if k == 0:
+        print(root.data)
+        return
+    printDepthK(root.left, k-1)
+    printDepthK(root.right, k-1)
+    
+def printDepthKv2(root,k,d=0):                      ##fn to print nodes at depth k version 2
+    if root == None:
+        return
+    if k == d:
+        print(root.data)
+        return
+    printDepthKv2(root.left , k , d+1)
+    printDepthKv2(root.right, k , d+1)
+    
+
+    
+btn1=BTnode(1)                                 #creating nodes
+btn2=BTnode(2)
+btn3=BTnode(3)
+btn4=BTnode(4)
+btn5=BTnode(5)
+btn1.left=btn2                                  #connecting edges
+btn1.right=btn3
+btn2.left=btn4
+btn2.right=btn5
+root = btn1                              #function call
+printTreeDetailed(root)
+treeHeight(root)
+
     
 btn1=BTnode(1)                                 #creating nodes
 btn2=BTnode(2)
