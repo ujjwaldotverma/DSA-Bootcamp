9614aea26063e2295a3ff18bbc3e437387efa3b4
ujjwaldotverma <72538299+ujjwaldotverma@users.noreply.github.com>
2022-11-21 19:53:22 +0530

    Update 01.ImplementationBinaryTree.py

--- 04.Binary Tree/01.ImplementationBinaryTree.py
+++ 04.Binary Tree/01.ImplementationBinaryTree.py
@@ -1,17 +1,17 @@
-class BTnode:
+class BTnode:                                   #constructor
     def __init__(self,data):
         self.data = data
         self.left = None
         self.right = None
         
-def printTree(root):
+def printTree(root):                            #print the tree
     if root == None:
         return
     print(root.data)
     printTree(root.left)
     printTree(root.right)
     
-def printTreeDetailed(root):
+def printTreeDetailed(root):                    #print but better
     if root == None:
         return
     print(root.data, end = ':')
@@ -23,13 +23,17 @@ def printTreeDetailed(root):
     printTreeDetailed(root.left)
     printTreeDetailed(root.right)
     
-btn1=BTnode(1)
+def treeInput():                            #ask user for input
+    rootData = input()
+    root = BT
+    
+btn1=BTnode(1)                                 #creating nodes
 btn2=BTnode(2)
 btn3=BTnode(3)
 btn4=BTnode(4)
 btn5=BTnode(5)
-btn1.left=btn2
+btn1.left=btn2                                  #connecting edges
 btn1.right=btn3
 btn2.left=btn4
 btn2.right=btn5
-printTreeDetailed(btn1)
+printTreeDetailed(btn1)                         #function call
